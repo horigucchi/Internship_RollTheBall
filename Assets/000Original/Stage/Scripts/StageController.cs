c49bc007702745b3ed7d5b4756305a9fed0a9d2f
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-18 14:49:37 +0900

    Add: stage controller script

---
+++ Assets/000Original/Stage/Scripts/StageController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    public const int WIDTH = 4;
+    public const int HEIGHT = 4;
+
+    [SerializeField]
+    private GameObject[] panels = new GameObject[WIDTH * HEIGHT];
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
