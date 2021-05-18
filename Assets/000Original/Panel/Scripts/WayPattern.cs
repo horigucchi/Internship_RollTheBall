6bd4b4cc3da21821840cee33935c5960a44bfe00
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-18 15:48:46 +0900

    Add: enum script about way pattern

---
+++ Assets/000Original/Panel/Scripts/WayPattern.cs
@@ -0,0 +1,19 @@
+public enum WayPattern
+{
+    None = 0b0000,
+    Right = 0b0001,
+    Down = 0b0010,
+    DownRight,
+    Left = 0b0100,
+    LeftRight,
+    LeftDown,
+    LeftDownRight,
+    Up = 0b1000,
+    UpRight,
+    UpDown,
+    UpDownRight,
+    UpLeft,
+    UpLeftRight,
+    UpLeftDown,
+    UpLeftDownRight,
+}
