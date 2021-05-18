96ced43a83f50183f0f8e1c392802b2956ba3c7a
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-18 17:54:04 +0900

    Update: panel script so that can move

--- Assets/000Original/Panel/Scripts/PanelController.cs
+++ Assets/000Original/Panel/Scripts/PanelController.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class PanelController : MonoBehaviour
 {
-    private PanelObject panelObject;
+    public PanelObject PanelObject { get; private set; }
+
     [SerializeField]
     PanelTextureTable textureTable;
 
+    // 移動中であるか
+    private bool isMoving = false;
+
+
     private void Awake()
     {
-        panelObject = GetComponent<PanelObject>();
+        PanelObject = GetComponent<PanelObject>();
     }
 
     private void Start()
     {
-        Sprite sprite = textureTable.GetSprite(panelObject.WayPattern);
-        panelObject.SetSprite(sprite);
+        Sprite sprite = textureTable.GetSprite(PanelObject.WayPattern);
+        PanelObject.SetSprite(sprite);
     }
+
+    /// <summary>
+    /// パネルを移動させる
+    /// </summary>
+    /// <param name="way">移動する方向</param>
+    /// <param name="callback">移動終了の通知</param>
+    public void Move(WayPattern way, System.Action callback = null)
+    {
+        Vector3 direction = Vector3.zero;
+        if ((way & WayPattern.Right) != 0) direction += Vector3.right;
+        if ((way & WayPattern.Down) != 0) direction += Vector3.down;
+        if ((way & WayPattern.Left) != 0) direction += Vector3.left;
+        if ((way & WayPattern.Up) != 0) direction += Vector3.up;
+
+        if (direction == Vector3.zero) return;
+        if (isMoving) return;
+        isMoving = true;
+        callback += () => { isMoving = false; };
+        StartCoroutine(PanelObject.TryMoving(direction, callback));
+    }
+
 
 
 }
--- Assets/000Original/Panel/Scripts/PanelObject.cs
+++ Assets/000Original/Panel/Scripts/PanelObject.cs
@@ -18,4 +18,30 @@ public class PanelObject : MonoBehaviour
     {
         spriteRenderer.sprite = sprite;
     }
+
+    /// <summary>
+    /// w’è‚Ì•ûŒü‚ÉˆÚ“®
+    /// </summary>
+    /// <param name="direction">ˆÚ“®‚·‚é•ûŒü</param>
+    /// <param name="callback">ˆÚ“®I—¹‚Ì’Ê’m</param>
+    /// <returns></returns>
+    public IEnumerator TryMoving(Vector3 direction, System.Action callback = null)
+    {
+        Vector3 firstPosition = transform.position;
+        Vector3 targetPosition = transform.position + direction;
+
+        const float SPEED = 2f;
+        float t = 0;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * SPEED;
+            Vector3 position = Vector3.Lerp(firstPosition, targetPosition, t);
+            transform.position = position;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        callback?.Invoke();
+        yield return null;
+    }
 }
