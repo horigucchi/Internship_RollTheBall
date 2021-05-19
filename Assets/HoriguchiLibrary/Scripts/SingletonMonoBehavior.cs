8be52a136a1aeed0fdbf310686ed22a4351f30f2
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-19 11:41:32 +0900

    Add: singleton script

---
+++ Assets/HoriguchiLibrary/Scripts/SingletonMonoBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    #region// singleton
+    private static T instance;
+
+    /// <summary>
+    /// シングルトン
+    /// </summary>
+    public static T Instance => instance = instance
+        ?? FindObjectOfType<T>() 
+        ?? new GameObject(typeof(T).Name).AddComponent<T>();
+
+    protected bool checkInstance()
+    {
+        if (Instance != this)
+        {
+            Destroy(this);
+            return false;
+        }
+        DontDestroyOnLoad(this);
+        return true;
+    }
+    #endregion
+
+    protected virtual void Awake()
+    {
+        checkInstance();
+    }
+}
