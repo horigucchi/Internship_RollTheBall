69fcc0c0b01fbd85595ec0938f13e7ed87b88d9f
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-19 18:00:34 +0900

    Add: UI/UX base

---
+++ Assets/000Original/Gameplay/GameplayUXController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUXController : MonoBehaviour
+{
+    [SerializeField]
+    private UIControllerBese ClearUI;
+
+    public void PlayClear(System.Action callback = null)
+    {
+        ClearUI.Show();
+        callback?.Invoke();
+    }
+}
---
+++ Assets/000Original/UI/Scripts/UIControllerBese.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerBese : MonoBehaviour
+{
+    [SerializeField]
+    private bool shouldBeginVisible = true;
+
+
+    protected virtual void Awake()
+    {
+        if (!shouldBeginVisible) Hide();
+    }
+
+    protected virtual void Start()
+    {
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
