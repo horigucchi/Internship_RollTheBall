a63c1b2e2c032bf333b8313aa46df7a79755df34
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-19 11:53:31 +0900

    Add: gameplay manager script

---
+++ Assets/000Original/Gameplay/GameplayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : SingletonMonoBehaviour<GameplayManager>
+{
+    public PlayerController PlayerController { get; private set; }
+    public StageController StageController { get; private set; }
+
+    private void attachComponent()
+    {
+        PlayerController = FindObjectOfType<PlayerController>();
+        StageController = FindObjectOfType<StageController>();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        attachComponent();
+    }
+
+    private void Start()
+    {
+        PlayerController.Swiped += onSwiped;
+    }
+
+    private void onSwiped(Vector3 position, WayPattern way)
+    {
+        StageController.SwipePanel(position, way);
+    }
+}
