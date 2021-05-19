93961627f318a3a1e2e8b3c81d02f476659b179c
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-19 11:50:49 +0900

    Update: stage controller so that can get the panel from the position

--- Assets/000Original/Stage/Scripts/StageController.cs
+++ Assets/000Original/Stage/Scripts/StageController.cs
@@ -46,10 +46,68 @@ public class StageController : MonoBehaviour
         return false;
     }
 
-    public void SwipePanel(int x, int y, WayPattern way)
+    /// <summary>
+    /// ゲームオブジェクトからパネルのインデックスを取得
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public Vector2Int GetIndex(GameObject obj)
     {
-        if (isOutOfRange(x, y)) return;
-        if (panelControllers[y, x] == null) return;
+        // FIX: パネルにインデックスを管理させた方がよさそう
+        int x = -1;
+        int y = -1;
+        for (int j = 0; j < HEIGHT; j++)
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                var item = panelControllers[j, i];
+                if (item == null) continue;
+                if (item.gameObject == obj)
+                {
+                    x = i;
+                    y = j;
+                    break;
+                }
+            }
+        }
+        return new Vector2Int(x, y);
+    }
+
+    /// <summary>
+    /// 座標からパネルのインデックスを取得
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    public Vector2Int GetIndex(Vector3 position)
+    {
+        Vector2Int index = new Vector2Int(-1, -1);
+
+        RaycastHit2D hit = Physics2D.Raycast(position, position);
+        if (hit)
+        {
+            index = GetIndex(hit.collider.gameObject);
+        }
+
+        return index;
+    }
+
+    public PanelController GetPanelController(int indexX, int indexY)
+    {
+        return panelControllers[indexY, indexX];
+    }
+
+    /// <summary>
+    /// インデックスからパネルを取得し動かす
+    /// </summary>
+    /// <param name="indexX"></param>
+    /// <param name="indexY"></param>
+    /// <param name="way"></param>
+    /// <returns></returns>
+    public bool SwipePanel(int indexX, int indexY, WayPattern way)
+    {
+        if ((way & WayPattern.UpLeftDownRight) == 0) return false;
+        if (isOutOfRange(indexX, indexY)) return false;
+        if (panelControllers[indexY, indexX] == null) return false;
 
         // 移動量
         int dx = 0;
@@ -61,27 +119,38 @@ public class StageController : MonoBehaviour
         if ((way & WayPattern.Left) != 0) dx -= 1;
 
         // 移動先配列座標
-        int indexX = x + dx;
-        int indexY = y + dy;
+        int targetIndexX = indexX + dx;
+        int targetIndexY = indexY + dy;
 
-        if (isOutOfRange(indexX, indexY)) return;
+        if (isOutOfRange(targetIndexX, targetIndexY)) return false;
         // 移動先にすでに置いてある場合中断
-        if (panelControllers[indexY, indexX] != null) return;
+        if (panelControllers[targetIndexY, targetIndexX] != null) return false;
 
+
+        // FIX: 非同期処理async/awaitを使った方がよさそう
         // 入れ替え
         System.Action swap = () =>
         {
-            var temp = panelControllers[indexY, indexX];
-            panelControllers[indexY, indexX] = panelControllers[y, x];
-            panelControllers[y, x] = temp;
+            var temp = panelControllers[targetIndexY, targetIndexX];
+            panelControllers[targetIndexY, targetIndexX] = panelControllers[indexY, indexX];
+            panelControllers[indexY, indexX] = temp;
         };
 
         // 移動の実行
-        panelControllers[y, x]?.Move(way, swap);
+        panelControllers[indexY, indexX]?.Move(way, swap);
+        return true;
     }
 
-    void Update()
+    /// <summary>
+    /// 座標からパネルを取得し動かす
+    /// </summary>
+    /// <param name="worldPosition"></param>
+    /// <param name="way"></param>
+    /// <returns></returns>
+    public bool SwipePanel(Vector3 worldPosition, WayPattern way)
     {
-        
+        Vector2Int index = GetIndex(worldPosition);
+        return SwipePanel(index.x, index.y, way);
     }
+
 }
