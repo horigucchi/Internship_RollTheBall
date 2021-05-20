4cc0fd81bf71eb22e5903b7d122d5e341b8c7a3e
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 17:40:57 +0900

    Add: game instance script

---
+++ Assets/000Original/GameInstance/Scripts/GameInstance.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : SingletonMonoBehaviour<GameInstance>
+{
+    public StageDataObject StageData { get; set; }
+}
