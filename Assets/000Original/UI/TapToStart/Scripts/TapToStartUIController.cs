9d50048eae6681175024ce60279657a1870a6c27
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 10:54:57 +0900

    Add: tap to start ui

---
+++ Assets/000Original/UI/TapToStart/Scripts/TapToStartUIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToStartUIController : UIControllerBase
+{
+    [SerializeField]
+    UnityEngine.UI.Text textUI;
+
+    private float blinkingSpeed = 2;
+
+    private bool isBreak = false;
+
+    private IEnumerator tryBlinkingText()
+    {
+        float t = 0;
+        Color firstColor = textUI.color;
+        Color blinkingColor = firstColor;
+        int sign = 1;
+        while (true)
+        {
+            if (isBreak) break;
+            textUI.color = blinkingColor;
+            blinkingColor.a = t;
+            t += blinkingSpeed * Time.deltaTime * sign;
+            if (t <= 0 || 1 <= t) sign *= -1;
+            yield return null;
+        }
+        textUI.color = firstColor;
+    }
+    private void playBlinkingText()
+    {
+        StartCoroutine(tryBlinkingText());
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        playBlinkingText();
+    }
+}
