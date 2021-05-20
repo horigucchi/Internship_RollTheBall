b9aac51a4f7f9fd1e4ad548ee622330a04691041
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 10:55:22 +0900

    Add: script to switch scene

---
+++ Assets/HoriguchiLibrary/Scripts/Module/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : SingletonMonoBehaviour<SceneSwitcher>
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
