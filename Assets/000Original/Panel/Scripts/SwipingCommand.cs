d53b17c235fbfa0d8a848255e92b5738d5f73dd9
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 14:34:39 +0900

    Add: swiping command script

---
+++ Assets/000Original/Panel/Scripts/SwipingCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipingCommand : IUndoable
+{
+    private System.Action undoing;
+    private System.Action redoing;
+
+    public SwipingCommand(System.Action undoing, System.Action redoing)
+    {
+        this.undoing = undoing;
+        this.redoing = redoing;
+    }
+
+    public void Undo()
+    {
+        undoing?.Invoke();
+    }
+
+    public void Redo()
+    {
+        redoing?.Invoke();
+    }
+
+}
