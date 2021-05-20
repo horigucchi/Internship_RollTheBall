6062937221e9b253d060eae86f5e0a190f88994c
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 18:05:06 +0900

    Add: panel skin data script

---
+++ Assets/000Original/Panel/Scripts/PanelSkinData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PanelSkinData
+{
+    [field: SerializeField]
+    public PanelTextureTable TextureTable { get; private set; }
+
+    [field: SerializeField]
+    public Color LockedColor { get; private set; } = new Color(0.8f, 0.8f, 0.8f);
+}
