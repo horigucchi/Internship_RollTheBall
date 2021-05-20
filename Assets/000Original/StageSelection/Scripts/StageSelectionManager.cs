717d4d8238a50590576821a7a294cbd966d3ca43
YukiHoriguchi <nkcgta.Yuki.Horiguchi@gmail.com>
2021-05-20 17:42:31 +0900

    Add: stage selection manager script

---
+++ Assets/000Original/StageSelection/Scripts/StageSelectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectionManager : MonoBehaviour
+{
+    public void SelectStage(StageDataObject stageData)
+    {
+        GameInstance.Instance.StageData = stageData;
+        SceneSwitcher.Instance.SwitchScene("GameScene");
+    }
+}
